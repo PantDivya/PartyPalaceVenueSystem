e192513a23ca8fa6a908daaede7c4dfad5714416
PantDivya <divyapant108@gmail.com>
2024-04-26 15:25:33 +0545

    Home background Image and login form added

--- PartyPalaceSystem/PartyPalaceSystem/obj/Release/net6.0/PartyPalaceSystem.AssemblyInfo.cs
+++ PartyPalaceSystem/PartyPalaceSystem/obj/Release/net6.0/PartyPalaceSystem.AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartyPalaceSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d5188e7e566890fa83071914876dabaed98ea00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartyPalaceSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartyPalaceSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
